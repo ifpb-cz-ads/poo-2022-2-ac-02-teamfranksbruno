353f7b5e6cd2ae651bf83d5ab6a7a0b7688647d3
Bruno Severo <brscabral07@gmail.com>
2022-10-13 22:10:27 -0300

    questões das paginas 59 até a 62

---
+++ RetanguloArea.java
@@ -0,0 +1,11 @@
+package atividade2;
+
+public class RetanguloArea {
+public static void main(String[] args) {
+	int base = 10;
+	int altura = 20;
+	float area = (base*altura);
+	System.out.println("A area do retangulo eh:" + area + " " + "m^2");
+	
+}
+}
